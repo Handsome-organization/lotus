ec68ea2781dbd2bbec8da0df5bb49af6bef1e1b3
amor <55129858+Amorous13@users.noreply.github.com>
2025-09-24 12:38:10 +0800

    Update ledger.go

--- chain/wallet/ledger/ledger.go
+++ chain/wallet/ledger/ledger.go
@@ -74,7 +74,7 @@ func (lw LedgerWallet) WalletSign(ctx context.Context, signer address.Address, t
 	}
 
 
-	sig, err := fl.Sign(ki.Path, meta.Extra, ledgerfil.SECP256K1,isMsg)
+	sig, err := fl.SignSECP256K1(ki.Path, meta.Extra, ledgerfil.SECP256K1,isMsg)
 	if err != nil {
 		return nil, err
 	}
